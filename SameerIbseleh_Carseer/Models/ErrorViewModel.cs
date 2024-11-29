28954cbd0e6e49a5a030c3b28ed174f3a43b4ce9
Sameer Ibseileh <sibseileh@transition-se.com>
2024-11-29 16:45:29 +0300

    Add project files.

---
+++ SameerIbseleh_Carseer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SameerIbseleh_Carseer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
